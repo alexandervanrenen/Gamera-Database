86f83248d58ec6b35ef00386c10c5a615a8d1867
alex <alexandervanrenen@gmail.com>
2013-05-27 13:58:52 +0200

    simple segment update test

--- test/segment_manager/SegmentManagerTest.cpp
+++ test/segment_manager/SegmentManagerTest.cpp
@@ -60,7 +60,7 @@ TEST(SegmentManager, PersistentSISingle)
       SegmentManager segmentManager(bufferManager, true);
       sid1 = segmentManager.createSegment(SegmentType::SP, 10);
       SPSegment& segment1 = segmentManager.getSPSegment(sid1);
-      tid = segment1.insert(Record("gemini wars crashed so i am back to coding :/"));
+      tid = segment1.insert(Record("Experience is simply the name we give our mistakes - Oscar Wilde"));
    }
 
    // Restart
@@ -69,7 +69,7 @@ TEST(SegmentManager, PersistentSISingle)
       BufferManager bufferManager(fileName, pages / 2);
       SegmentManager segmentManager(bufferManager, false);
       SPSegment& segment1 = segmentManager.getSPSegment(sid1);
-      ASSERT_EQ(segment1.lookup(tid), Record("gemini wars crashed so i am back to coding :/"));
+      ASSERT_EQ(segment1.lookup(tid), Record("Experience is simply the name we give our mistakes - Oscar Wilde"));
    }
 
    remove(fileName.c_str());
@@ -159,6 +159,50 @@ TEST(SegmentManager, SPSegmentSimple)
    segment.remove(tid);
 
    remove(fileName.c_str());
+}
+
+TEST(SegmentManager, SPSegmentManyPageUpdate)
+{
+   const string fileName = "swap_file";
+   const uint32_t pages = 100;
+   const Record smallRecord = Record("the tree of liberty must be refreshed from time to time with the blood of patriots and tyrants. it is it's natural manure.");
+   const Record bigRecord1 = Record(string(kPageSize/2, 'a'));
+   const Record bigRecord2 = Record(string(kPageSize/2, 'b'));
+
+   // Create
+   ASSERT_TRUE(util::createFile(fileName, pages * kPageSize));
+   BufferManager bufferManager(fileName, pages / 2);
+   SegmentManager segmentManager(bufferManager, true);
+   SegmentId id = segmentManager.createSegment(SegmentType::SP, 10);
+   SPSegment& segment = segmentManager.getSPSegment(id);
+
+   // Establish structure: <12 Byte> <kPageSize/2>
+   TId tid1 = segment.insert(smallRecord);
+   TId tid2 = segment.insert(bigRecord2);
+
+   // Now trigger a non page local update by increasing the 12 Byte to kPageSize/2 byte
+   segment.update(tid1, bigRecord1);
+
+   // Check result
+   ASSERT_EQ(bigRecord1, segment.lookup(tid1));
+   ASSERT_EQ(bigRecord2, segment.lookup(tid2));
+   TableScanOperator scanner(segment);
+   scanner.open();
+   while(scanner.next()) {
+      const pair<TId, Record>& record = scanner.getOutput();
+      ASSERT_TRUE(record.first == tid1 || record.first == tid2);
+      if(record.first == tid1)
+         ASSERT_TRUE(record.second == bigRecord1); else
+         ASSERT_TRUE(record.second == bigRecord2);
+   }
+   scanner.close();
+
+   // Update back to smaller record
+
+
+   // Remove both records
+   // segment.remove(tid1);
+   // segment.remove(tid2);
 }
 
 // TEST(SegmentManager, FunkeTest)

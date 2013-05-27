462bb0ce232361cbfdb1291289433e3aef4f8fba
alex <alexandervanrenen@gmail.com>
2013-05-28 00:57:02 +0200

    documented some limits

--- src/common/Config.hpp
+++ src/common/Config.hpp
@@ -24,17 +24,24 @@ inline TId toTID(PageId pid, RecordId rId)
    return (pid << 16) + rId;
 }
 
-static const uint32_t kPageSize = 1 << 14; // less than 1<<14 .. otherwise slotted page breaks (terribly(!))
+/// The size of a page, has to be a power of two and between 4KB and 32KB
+static const uint32_t kPageSize = 1 << 14;
 
 static const TId kInvalidTupleID = -1;
 static const PageId kInvalidPageID = -1;
 static const RecordId kInvalidRecordID = -1;
 
+/// Use same id for free space inventory
 static const SegmentId kFreeSpaceInventoryId = 0;
 
+/// The first page in a linked list like structure of pages used to store the SI.
 static const PageId kMetaPageId = 0;
+/// The first page which is not used by any database pages (till now only kMetaPage).
 static const PageId kFirstFreePageId = 1;
 
+/// The size of the record is limited
+/// Minimum: In order to allow for updates it has to be possible to replace any record by a reference.
+/// Maximum: Each record has to fit on one page.
 static const uint16_t kMinimumRecordSize = 8;
 static const uint16_t kMaximumRecordSize = kPageSize - 16 - 4; // 16 == Slotted page header; 4 == slot
 }

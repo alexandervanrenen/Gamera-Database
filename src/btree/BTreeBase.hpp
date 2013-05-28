e5d7120f13f9a1ead15e000977eb7617a76a3446
Sebastian Woehrl <sebastian.woehrl@mytum.de>
2013-05-28 19:17:06 +0200

    Adopt BTree to new PageId and TupleID classes

--- src/btree/BTreeBase.hpp
+++ src/btree/BTreeBase.hpp
@@ -1,11 +1,14 @@
 #ifndef BTREEBASE_HPP
 #define BTREEBASE_HPP
 
+#include "common/Config.hpp"
+
 namespace dbi {
 
-const uint64_t PAGESIZE = 4*1024;
-typedef uint64_t PageId;
-typedef uint64_t TID;
+const uint64_t PAGESIZE = kPageSize;//4*1024;
+//typedef uint64_t PageId;
+//typedef uint64_t TID;
+typedef TupleId TID;
 
 }
 

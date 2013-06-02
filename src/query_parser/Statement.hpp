785e9f2781c3a161452a4ef44212b2a82cc3e429
alex <alexandervanrenen@gmail.com>
2013-06-02 10:37:55 +0200

    builing a tree

--- src/query_parser/Statement.hpp
+++ src/query_parser/Statement.hpp
@@ -8,25 +8,33 @@ namespace dbi {
 
 namespace script {
 
+/// In "select s.name from Students s;" the term "s.name" is a ColumnIdentifier.
+struct ColumnIdentifier { std::string tableIdentifier; std::string columnIdentifier; };
+
+/// In "select s.name from Students s;" the term "Students s" is a TableAccess.
+struct TableAccess { std::string tableIdentifier; std::string alias; };
+
+// In "create table Students(id integer not null)" the term "id integer not null" as an AttributeDeclaration.
+struct AttributeDeclaration { std::string name; std::string type; bool notNull; void dump() {std::cout << name << " " << type << " " << notNull << std::endl;} };
+
 struct Statement {
    virtual void dump() = 0;
 };
 
 struct SelectStatement : public Statement {
 
-   struct Selector { std::string tableIdentifier; std::string columnIdentifier; };
-   std::vector<Selector> selectors;
+   SelectStatement(std::vector<ColumnIdentifier> selectors, std::vector<TableAccess> sources) : selectors(selectors), sources(sources) {}
 
-   struct Source { std::string tableIdentifier; std::string alias; };
-   std::vector<Source> sources;
+   std::vector<ColumnIdentifier> selectors;
+   std::vector<TableAccess> sources;
 
    virtual void dump() {
       std::cout << "---------" << std::endl;
       std::cout << "select " << std::endl;
-      for(auto iter : selectors)
+      for(auto& iter : selectors)
          std::cout << "    " << iter.tableIdentifier << " " << iter.columnIdentifier << std::endl;
       std::cout << "from " << std::endl;
-      for(auto iter : sources)
+      for(auto& iter : sources)
          std::cout << "    " << iter.tableIdentifier << " " << iter.alias << std::endl;
       std::cout << "---------" << std::endl;
    }
@@ -34,15 +42,11 @@ struct SelectStatement : public Statement {
 
 struct CreateStatement : public Statement {
 
-   struct Attribute {
-      std::string name;
-      std::string type;
-      bool notNull;
-      void dump() {std::cout << name << " " << type << " " << notNull << std::endl;}
-   };
+   CreateStatement(const std::string& name, std::vector<AttributeDeclaration>& attributes) : name(name), attributes(attributes) {}
+
    std::string name;
-   std::vector<Attribute> attributes;
-   std::vector<unsigned> primaryKey;
+   std::vector<AttributeDeclaration> attributes;
+   // std::vector<unsigned> primaryKey;
 
    virtual void dump() {
       std::cout << "---------" << std::endl;

689292e66d4242ad16b0338644904ae26c891a8f
alexander van renen <alexandervanrenen@gmail.com>
2013-06-22 22:50:16 +0200

    more query tests

--- sql/schema/big_bang_theory.sql
+++ sql/schema/big_bang_theory.sql
@@ -6,10 +6,12 @@ insert into Persons values(2, 'Sheldon', 'Cooper', 'Physics');
 insert into Persons values(3, 'Rajesh', 'koothrappali', 'Astrophysics');
 insert into Persons values(4, 'Howard', 'Wolowitz', 'Engineer');
 
-create table Relations (type char(20), lhs char(20), rhs char(20));
+create table Relations (type char(20), lhs integer, rhs integer);
 
-insert into Relations values('best friends', 'Lenard', 'Sheldon');
-insert into Relations values('best friends', 'Howard', 'Rajesh');
-insert into Relations values('sex', 'Penny', 'Lenard');
+insert into Relations values('best friends', 0, 2);
+insert into Relations values('best friends', 4, 3);
+insert into Relations values('sex', 1, 0);
 
-select firstname from Persons where false = true;
+select p1.firstname
+from Persons p1
+where p1.id = 2;
--- test/query/QueryTest.cpp
+++ test/query/QueryTest.cpp
@@ -6,115 +6,91 @@
 #include "query/result/QueryResultCollection.hpp"
 #include "harriet/Value.hpp"
 #include "util/Utility.hpp"
+#include "QueryTestUtil.hpp"
 #include <set>
 
 using namespace std;
 using namespace dbi;
 
-namespace {
-   bool valid;
-   void googleWrapper(unique_ptr<QueryResultCollection> result, const vector<vector<harriet::Value>>& expected)
-   {
-      ASSERT_TRUE(result->getAsSelect() != nullptr);
-      auto& selectResult = *result->getAsSelect();
-      ASSERT_TRUE(selectResult.result.size() == expected.size());
-      for(uint32_t row=0; row<expected.size(); row++) {
-         ASSERT_TRUE(selectResult.result[row].size() == expected[row].size());
-         for(uint32_t column=0; column<expected[row].size(); column++)
-            ASSERT_EQ(selectResult.result[row][column], expected[row][column]);
-      }
-      valid = true;
-   }
-   bool validateSelection(unique_ptr<QueryResultCollection> result, const vector<vector<harriet::Value>>& expected)
-   {
-      valid = false;
-      googleWrapper(move(result), expected);
-      return valid;
-   }
+struct QueryTest : public testing::Test {
+
+  virtual void SetUp() {
+     // Start database
+     DatabaseConfig config{kSwapFileName, kSwapFilePages};
+     database = util::make_unique<Database>(config, true);
+     auto query = util::loadFileToMemory("sql/schema/big_bang_theory.sql");
+     ASSERT_TRUE(database->executeQuery(query)->good());
+
+     // Prepare results
+     expectedLen.resize(1);
+     expectedLen[0].emplace_back(harriet::Value::createCharacter(string("Lenard"), 10));
+
+     expectedAll.resize(5);
+     expectedAll[0].emplace_back(lenard.createCopy());
+     expectedAll[1].emplace_back(harriet::Value::createCharacter(string("Penny"), 10));
+     expectedAll[2].emplace_back(harriet::Value::createCharacter(string("Sheldon"), 10));
+     expectedAll[3].emplace_back(harriet::Value::createCharacter(string("Rajesh"), 10));
+     expectedAll[4].emplace_back(harriet::Value::createCharacter(string("Howard"), 10));
+  }
+
+  harriet::Value lenard = harriet::Value::createCharacter(string("Lenard"), 10);
+
+  vector<vector<harriet::Value>> expectedLen;
+  vector<vector<harriet::Value>> expectedAll;
+  vector<vector<harriet::Value>> expectedNone;
+  unique_ptr<Database> database;
+};
+
+TEST_F(QueryTest, SelectionGood)
+{
+   auto& db = *database;
+
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard';"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where Persons.firstname = 'Lenard';"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where 'Lenard' = firstname;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where 'Lenard' = Persons.firstname;"), expectedLen));
+
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and firstname = 'Lenard';"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and lastname = 'Hofstadter';"), expectedLen));
+
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and 2 = 2;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where 2 = 2 and firstname = 'Lenard';"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and 2 = 3;"), expectedNone));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where 2 = 3 and firstname = 'Lenard';"), expectedNone));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where false;"), expectedNone));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where false = true;"), expectedNone));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where true = true;"), expectedAll));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where true;"), expectedAll));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where true = false = false;"), expectedAll));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where true = true = false;"), expectedNone));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons;"), expectedAll));
+
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and (id+1)*2*2 = 4;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and id=id;"), expectedLen));
 }
 
-TEST(Query, GoodRoundTrip)
+TEST_F(QueryTest, ProjectionGood)
 {
-   DatabaseConfig config{kSwapFileName, kSwapFilePages};
+   auto& db = *database;
 
-   // Start database
-   Database db(config, true);
-   ASSERT_TRUE(db.executeQuery("create table Persons (name char(20), job char(20));")->good());
-   ASSERT_TRUE(db.executeQuery("insert into Persons values('Lenard', 'Physics');")->good());
-   auto queryResultCollection = db.executeQuery("select name from Persons;");
+   vector<vector<harriet::Value>> expectedTwo(1);
+   expectedTwo[0].emplace_back(lenard.createCopy());
+   expectedTwo[0].emplace_back(lenard.createCopy());
 
-   // Check query
-   vector<vector<harriet::Value>> result(1);
-   result[0].emplace_back(harriet::Value::createCharacter(string("Lenard"), 10));
-   ASSERT_TRUE(validateSelection(move(queryResultCollection), result));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons where id = 0;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select Persons.firstname from Persons where id = 0;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select p.firstname from Persons p where id = 0;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname from Persons p where id = 0;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select firstname, firstname from Persons where id = 0;"), expectedTwo));
+   ASSERT_TRUE(validateResult(db.executeQuery("select p.firstname, firstname from Persons p where id = 0;"), expectedTwo));
 }
 
-TEST(Query, GoodRoundTripWithRestart)
+TEST_F(QueryTest, JoinsGood)
 {
-   DatabaseConfig config{kSwapFileName, kSwapFilePages};
+   auto& db = *database;
 
-   // Start database
-   {
-      Database db(config, true);
-      ASSERT_TRUE(db.executeQuery("create table Persons (name char(20), job char(20));")->good());
-      ASSERT_TRUE(db.executeQuery("insert into Persons values('Lenard', 'Physics');")->good());
-      auto queryResultCollection = db.executeQuery("select name from Persons;");
-
-      // Check query
-      vector<vector<harriet::Value>> result(1);
-      result[0].emplace_back(harriet::Value::createCharacter(string("Lenard"), 10));
-      ASSERT_TRUE(validateSelection(move(queryResultCollection), result));
-   }
-
-   // Restart database
-   {
-      Database db(config, false);
-      auto queryResultCollection = db.executeQuery("select name from Persons;");
-
-      // Check query
-      vector<vector<harriet::Value>> result(1);
-      result[0].emplace_back(harriet::Value::createCharacter(string("Lenard"), 10));
-      ASSERT_TRUE(validateSelection(move(queryResultCollection), result));
-   }
-}
-
-TEST(Query, Selection)
-{
-   DatabaseConfig config{kSwapFileName, kSwapFilePages};
-
-   // Start database
-   Database db(config, true);
-   auto query = util::loadFileToMemory("sql/schema/big_bang_theory.sql");
-   ASSERT_TRUE(db.executeQuery(query)->good());
-
-   vector<vector<harriet::Value>> expectedLen(1);
-   expectedLen[0].emplace_back(harriet::Value::createCharacter(string("Lenard"), 10));
-
-   vector<vector<harriet::Value>> expectedAll(5);
-   expectedAll[0].emplace_back(harriet::Value::createCharacter(string("Lenard"), 10));
-   expectedAll[1].emplace_back(harriet::Value::createCharacter(string("Penny"), 10));
-   expectedAll[2].emplace_back(harriet::Value::createCharacter(string("Sheldon"), 10));
-   expectedAll[3].emplace_back(harriet::Value::createCharacter(string("Rajesh"), 10));
-   expectedAll[4].emplace_back(harriet::Value::createCharacter(string("Howard"), 10));
-
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard';"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where Persons.firstname = 'Lenard';"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where 'Lenard' = firstname;"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where 'Lenard' = Persons.firstname;"), expectedLen));
-
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and firstname = 'Lenard';"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and lastname = 'Hofstadter';"), expectedLen));
-
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and 2 = 2;"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where 2 = 2 and firstname = 'Lenard';"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and 2 = 3;"), vector<vector<harriet::Value>>(0)));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where 2 = 3 and firstname = 'Lenard';"), vector<vector<harriet::Value>>(0)));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where false;"), vector<vector<harriet::Value>>(0)));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where false = true;"), vector<vector<harriet::Value>>(0)));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where true = true;"), expectedAll));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where true;"), expectedAll));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons;"), expectedAll));
-
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and (id+1)*2*2 = 4;"), expectedLen));
-   ASSERT_TRUE(validateSelection(db.executeQuery("select firstname from Persons where firstname = 'Lenard' and id=id;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select p1.firstname from Persons p1, Persons p2 where p1.id = p2.id;"), expectedAll));
+   ASSERT_TRUE(validateResult(db.executeQuery("select p1.firstname from Persons p1, Persons p2 where p1.id = p2.id and p1.id = 0;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select p1.firstname from Persons p1, Persons p2 where p1.id = p2.id and p2.id = 0;"), expectedLen));
+   ASSERT_TRUE(validateResult(db.executeQuery("select p2.firstname from Persons p1, Relations r, Persons p2 where p1.firstname = 'Penny' and r.type = 'sex' and r.lhs = p1.id and r.rhs = p2.id;"), expectedLen));
 }

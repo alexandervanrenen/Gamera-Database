77e519bb95a3d9be5d4930c969ce2525442da2bd
alex <alexandervanrenen@gmail.com>
2017-02-19 22:17:29 +0100

    kicked webinterface stuff

--- web/test_driver.rb
+++
@@ -1,9 +0,0 @@
-# Runs all tests from the test directory
-# -----------------------------------------------
-require 'test/unit'
-require './validator.rb'
-# -----------------------------------------------
-for test_case in Dir["./test_cases/*.rb"] do
-  require test_case
-end
-# -----------------------------------------------
